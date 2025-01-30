3ba9883520797c06e8bbcdfd1eb514f76c8d19e6
Tuncay Ekmez <tuncayekmez@gmail.com>
2025-01-30 19:25:12 +0300

    fix: image picker types

--- app/(main)/edit.tsx
+++ app/(main)/edit.tsx
@@ -13,7 +13,7 @@ export default function Edit() {
 
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: "images",
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
